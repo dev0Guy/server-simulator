f112658a426975a9e4f2280ec7f90cf0ca76de6c
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2025-11-28 23:47:35 +0200

    add initialize

--- src/cluster/core/machine.py
+++ src/cluster/core/machine.py
@@ -2,12 +2,22 @@ import abc
 import typing as tp
 
 T = tp.TypeVar("T")
+
 ObsType = tp.TypeVar("ObsType")
 
 
 @tp.runtime_checkable
 class Machine(tp.Protocol[T]):
     free_space: T
+
+# @tp.runtime_checkable
+# class DilationOperation:
+#
+#     @abc.abstractmethod
+#     def zoom_in(self, cell: tp.Tuple[int, ...]) -> tp.Self: ...
+#
+#     @abc.abstractmethod
+#     def zoom_out(self) -> tp.Self: ...
 
 
 @tp.runtime_checkable
@@ -30,3 +40,11 @@ class MachineCollection(tp.Protocol[T]):
 
     @abc.abstractmethod
     def get_observation(self) -> ObsType: ...
+
+# M = tp.TypeVar('M', MachineCollection, DilationOperation)
+#
+# @tp.runtime_checkable
+# class MachineCollectionDilationCreator:
+#
+#     @staticmethod
+#     def cast_into_dilation_collection(machines: MachineCollection[T]) -> M: ...

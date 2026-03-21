23904d98b2041c37af943fc90ce2a0fe54b6af94
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2026-03-21 15:41:12 +0200

    add import

--- tests/test_e2e/test_e2e_single_slot.py
+++ tests/test_e2e/test_e2e_single_slot.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 from gymnasium.utils.env_checker import check_env
-import src.server_simulator
+import src.server_simulator # noqa: F401
 
 env = gym.make("ClusterScheduling-single-slot-v1").unwrapped
 check_env(env)
